8f0f54117441bd613ee736bd1299a946f4ba2791
baesangbum <yeppbaby91@naver.com>
2015-08-02 18:44:08 +0900

    deafault activity edit

--- app/src/main/java/com/example/yeppb/subway_please/MainActivity.java
+++ app/src/main/java/com/example/yeppb/subway_please/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     Button btnComm;
@@ -48,6 +49,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     public void onClick(View v) {
-
+        //if(v.getId().value == btnComm) {
+            Toast.makeText(MainActivity.this, "1번", Toast.LENGTH_SHORT).show();
+        //}
     }
 }
